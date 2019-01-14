44184f661f0235e709e40d485915fbbbc60a3827
duxulin <869330465@qq.com>
2019-01-14 22:54:43 +0800

    add

---
+++ config-client/src/main/java/com/dxl/config/client/MyConfigClientApplication.java
@@ -0,0 +1,22 @@
+package com.dxl.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MyConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyConfigClientApplication.class,args);
+	}
+	@Value("${foo}")
+	String foo;
+	@RequestMapping("/hi")
+	public String hi(){
+		return foo;
+	}
+}
